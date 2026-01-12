f5002d81c51a8caf65785b8d94bb4ffc7c296f96
simon <simon.hill11@gmail.com>
2026-01-12 17:49:28 +1100

    Updated moose.lua

--- resources/plugins/airboss/airboss.lua
+++ resources/plugins/airboss/airboss.lua
@@ -503,6 +503,7 @@ function SetupAirboss(nameOfCarrier, carrierType)
     AirbossRetribution:SetCollisionDistance(15)
     AirbossRetribution:SetCarrierIllumination(-1)
     AirbossRetribution:SetExtraVoiceOvers(true)
+    AirbossRetribution:SetExtraVoiceOversAI(true)
     ReportDayNightStatusAtBullseye()
 
     if MSRS_Config then
