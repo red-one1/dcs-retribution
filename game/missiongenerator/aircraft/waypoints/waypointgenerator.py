012a8f6f785cda3127d2ee9cb44c903d7b1876ac
simon <simon.hill11@gmail.com>
2026-01-22 19:13:46 +1100

    Add TakeoffPointBuilder for TARCAP self-defense tasks and update waypoint generator

--- game/missiongenerator/aircraft/waypoints/waypointgenerator.py
+++ game/missiongenerator/aircraft/waypoints/waypointgenerator.py
@@ -42,6 +42,7 @@ from .seadsweepingress import SeadSweepIngressBuilder
 from .splitpoint import SplitPointBuilder
 from .strikeingress import StrikeIngressBuilder
 from .sweepingress import SweepIngressBuilder
+from .takeoff import TakeoffPointBuilder
 from .target import TargetBuilder
 
 
@@ -157,6 +158,7 @@ class WaypointGenerator:
             FlightWaypointType.SPLIT: SplitPointBuilder,
             FlightWaypointType.TARGET_GROUP_LOC: TargetBuilder,
             FlightWaypointType.TARGET_POINT: TargetBuilder,
+            FlightWaypointType.TAKEOFF: TakeoffPointBuilder,
         }
         builder = builders.get(waypoint.waypoint_type, DefaultWaypointBuilder)
         return builder(
